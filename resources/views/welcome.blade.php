08d02f9a202995f6a39019291867aaf8d12bd802
Jose-pe <joseluis.von@gmail.com>
2026-07-23 16:28:22 -0500

    refresh localstorage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -440,7 +440,7 @@
     </div>
   
 </footer>
-
+<script src="/js/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" defer></script>
 <script src="https://unpkg.com/aos@next/dist/aos.js" defer></script>
 
